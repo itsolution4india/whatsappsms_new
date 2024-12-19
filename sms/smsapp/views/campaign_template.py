608024bc1ef8ae41b05b517b7b7186ebc0c1eb6e
Prashanthg01 <prashanthprashanth8585@gmail.com>
2024-12-19 18:14:17 +0530

    Updated views

---
+++ sms/smsapp/views/campaign_template.py
@@ -0,0 +1,170 @@
+from ..models import TemplateLinkage, Templates
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from ..create_template import template_create, create_auth_template
+from ..functions.template_msg import fetch_templates, delete_whatsapp_template
+from ..functions.send_messages import display_phonenumber_id
+from ..utils import parse_fb_error, get_token_and_app_id, display_whatsapp_id
+from .auth import check_user_permission, username
+from ..functions.template_msg import header_handle
+import time
+from ..media_id import process_media_file
+
+@login_required
+def Campaign(request):
+    if not check_user_permission(request.user, 'can_manage_campaign'):
+        return redirect("access_denide")
+    token, app_id = get_token_and_app_id(request)
+    campaign_list = fetch_templates(display_whatsapp_id(request), token)
+    if campaign_list is None :
+        campaign_list=[]
+    template_database = Templates.objects.filter(email=request.user)
+    template_value = list(template_database.values_list('templates', flat=True))
+    templates = [campaign_list[i] for i in range(len(campaign_list)) if campaign_list[i]['template_name'] in template_value]
+
+    context = {
+        "template_value": template_value,
+        "coins":request.user.marketing_coins + request.user.authentication_coins,
+        "marketing_coins":request.user.marketing_coins,
+        "authentication_coins":request.user.authentication_coins,
+        "username": username(request),
+        "WABA_ID": display_whatsapp_id(request),
+        "PHONE_ID": display_phonenumber_id(request),
+        "campaign_list": templates,
+        
+    }
+
+    if request.method == 'POST':
+        template_name = request.POST.get('template_name')
+        language = request.POST.get('language')
+        category = request.POST.get('category')
+        header_type = request.POST.get('actionHeaderType')
+        header_content = None
+        submitted_variables = []
+        for key in request.POST:
+                if key.startswith('var_input'):
+                    submitted_variables.append(request.POST[key])
+                    
+        if header_type == 'headerText':
+            header_content = request.POST.get('headerText', None)
+        elif header_type == 'headerImage':
+            header_content = request.FILES.get('headerImage', None)
+        elif header_type == 'headerVideo':
+            header_content = request.FILES.get('headerVideo', None)
+        elif header_type == 'headerDocument':
+            header_content = request.FILES.get('headerDocument', None)
+        elif header_type == 'headerDocument':
+            header_content = request.FILES.get('headerAudio', None)
+        
+        body_text = request.POST.get('template_data').replace('\n', '\n').replace('<b>', '*').replace('</b>', '*')
+        footer_text = request.POST.get('footer_data')
+        call_button_text = request.POST.get('callbutton', None)
+        phone_number = request.POST.get('contactNumber', None)
+        quick_reply_one = request.POST.get('quick_reply_one', None)
+        quick_reply_two = request.POST.get('quick_reply_two', None)
+        quick_reply_three = request.POST.get('quick_reply_three', None)
+        url_button_text = request.POST.get('websitebutton', None)
+        website_url = request.POST.get('websiteUrl')
+        linked_temp_one = request.POST.get('linked_temp_one', None)
+        linked_temp_two = request.POST.get('linked_temp_two', None)
+        linked_temp_three = request.POST.get('linked_temp_three', None)
+
+        media_file_one = request.FILES.get('file_one', None)
+        media_file_two = request.FILES.get('file_two', None)
+        media_file_three = request.FILES.get('file_three', None)
+
+        if media_file_one:
+            media_id_one, media_type_one = process_media_file(media_file_one, display_phonenumber_id(request), token)
+            time.sleep(1.5) 
+        else:
+            media_id_one, media_type_one = None, None
+
+        if media_file_two:
+            media_id_two, media_type_two = process_media_file(media_file_two, display_phonenumber_id(request), token)
+            time.sleep(1.5) 
+        else:
+            media_id_two, media_type_two = None, None
+
+        if media_file_three:
+            media_id_three, media_type_three = process_media_file(media_file_three, display_phonenumber_id(request), token)
+        else:
+            media_id_three, media_type_three = None, None
+
+        media_id_one = media_id_one + '|' + media_type_one if media_id_one else None
+        media_id_two = media_id_two + '|' + media_type_two if media_id_two else None
+        media_id_three = media_id_three + '|' + media_type_three if media_id_three else None
+
+        if header_type in ['headerImage','headerVideo','headerDocument','headerAudio']:
+            header_content = header_handle(header_content, token, app_id)
+        
+        if quick_reply_one and linked_temp_one:
+            TemplateLinkage.objects.create(template_name=template_name, linked_template_name=linked_temp_one, button_name=quick_reply_one, useremail=request.user.email, image_id=media_id_one)
+
+        if quick_reply_two and linked_temp_two:
+            TemplateLinkage.objects.create(template_name=template_name, linked_template_name=linked_temp_two, button_name=quick_reply_two, useremail=request.user.email, image_id=media_id_two)
+
+        if quick_reply_three and linked_temp_three:
+            TemplateLinkage.objects.create(template_name=template_name, linked_template_name=linked_temp_three, button_name=quick_reply_three, useremail=request.user.email, image_id=media_id_three)
+            
+        try:
+            if category == 'Authentication':
+                status, response = create_auth_template(
+                    waba_id=display_whatsapp_id(request),
+                    access_token=token,
+                    template_name=template_name,
+                    languages=language
+                )
+            else:
+                status,data=template_create(
+                    token=token,
+                    waba_id=display_whatsapp_id(request),
+                    template_name=template_name,
+                    language=language,
+                    category=category,
+                    header_type=header_type,
+                    header_content=header_content,
+                    body_text=body_text,
+                    footer_text=footer_text,
+                    call_button_text=call_button_text,
+                    phone_number=phone_number,
+                    url_button_text=url_button_text,
+                    website_url=website_url,
+                    quick_reply_one=quick_reply_one,
+                    quick_reply_two=quick_reply_two,
+                    quick_reply_three=quick_reply_three,
+                    body_example_values = submitted_variables if submitted_variables else None
+                )
+            if status !=200:
+                error_details = parse_fb_error(data)
+                context.update({
+                    "error": error_details,
+                    "has_error": True
+                })
+
+            Templates.objects.create(email=request.user, templates=template_name)
+        except Exception as e:
+            context.update({
+                "error": {
+                    "type": "Unexpected Error",
+                    "message": str(e),
+                    "details": str(e)
+                },
+                "has_error": True
+            })
+        
+    return render(request, "Campaign.html", context)
+
+@login_required
+def delete_template(request):
+    token, _ = get_token_and_app_id(request)
+    template_name = request.POST.get('template_name')
+    template_id = request.POST.get('template_id')
+
+    delete_result = delete_whatsapp_template(waba_id=display_whatsapp_id(request), token=token, template_name=template_name, template_id=template_id)
+    
+    if delete_result:
+        print(f"Template '{template_name}' deleted successfully.")
+    else:
+        print(f"Failed to delete template '{template_name}'.")
+    
+    return redirect('campaign')
---
+++ sms/smsapp/views/reports.py
@@ -0,0 +1,360 @@
+from .auth import check_user_permission, username
+from ..models import Templates, ReportInfo
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from ..utils import logger, display_whatsapp_id, display_phonenumber_id
+import pandas as pd
+import plotly.express as px
+import mysql.connector
+from datetime import datetime
+import csv, copy, random
+from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_http_methods
+
+
+@login_required
+def Reports(request):
+    if not check_user_permission(request.user, 'can_view_reports'):
+        return redirect("access_denide")
+    try:
+        template_database = Templates.objects.filter(email=request.user)
+        template_value = list(template_database.values_list('templates', flat=True))
+        report_list = ReportInfo.objects.filter(email=request.user)
+        # df = pd.read_csv(r"C:\Users\user\Downloads\webhook_responses.csv")
+        df = download_linked_report(request)
+        df['contact_wa_id'] = df['contact_wa_id'].astype(str)
+        df['contact_wa_id'] = df['contact_wa_id'].str.replace(r'\.0$', '', regex=True)
+        
+        df['phone_number_id'] = df['phone_number_id'].astype(str)
+        df['phone_number_id'] = df['phone_number_id'].str.replace(r'\.0$', '', regex=True)
+        df['phone_number_id'] = pd.to_numeric(df['phone_number_id'], errors='coerce', downcast='integer').astype('Int64')
+
+        filterd_df = df[df['phone_number_id'] == int(display_phonenumber_id(request))]
+
+        all_phone_numbers = []
+        for report in report_list:
+            phone_numbers = report.contact_list.split(',')
+            all_phone_numbers.extend(phone_numbers)
+        all_phone_numbers = list(set(all_phone_numbers))
+
+        filtered_df = filterd_df[filterd_df['contact_wa_id'].isin(all_phone_numbers)]
+        filtered_df = filtered_df.sort_values(by='Date', ascending=False)
+        filtered_df = filtered_df.drop_duplicates(subset='waba_id', keep='first')
+        unique_count = filtered_df['contact_wa_id'].nunique()
+        total_count = filtered_df['contact_wa_id'].count()
+        status_counts = filtered_df['status'].value_counts()
+        status_df = status_counts.reset_index()
+        status_df.columns = ['Status', 'Counts']
+        total_conversations_df = pd.DataFrame({'Status': ['Total Conversations'], 'Counts': [total_count]})
+        total_contacts_df = pd.DataFrame({'Status': ['Total_Contacts'], 'Counts': [unique_count]})
+        status_df = pd.concat([status_df, total_conversations_df, total_contacts_df], ignore_index=True)
+
+        status_df = status_df[status_df['Status'] != 'Total_Contacts']
+        status_list = status_df.to_dict(orient='records')
+        status_df = status_df[status_df['Status'] != 'Total Conversations']
+        labels = status_df['Status'].tolist()
+        values = status_df['Counts'].tolist()
+        fig = px.pie(status_df, names=labels, values=values)
+        fig.update_layout(width=600, height=400)
+        pie_chart = fig.to_json()
+        context = {
+            "template_names": template_value,
+            "coins":request.user.marketing_coins + request.user.authentication_coins,
+            "marketing_coins":request.user.marketing_coins,
+            "authentication_coins":request.user.authentication_coins,
+            "username": username(request),
+            "WABA_ID": display_whatsapp_id(request),
+            "PHONE_ID": display_phonenumber_id(request),
+            "report_list":report_list,
+            "status_list": status_list,
+            "pie_chart": pie_chart
+            }
+        
+
+        return render(request, "reports.html", context)
+    except Exception as e:
+        
+        return render(request, "reports.html", context)
+    
+@login_required
+def download_linked_report(request, button_name=None, start_date=None, end_date=None):
+    try:
+        # Connect to the database
+        connection = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="fedqrbtb_wtsdealnow",
+            password="Solution@97",
+            database="fedqrbtb_report",
+            auth_plugin='mysql_native_password'
+        )
+        cursor = connection.cursor()
+        
+        # Base query
+        query = "SELECT * FROM webhook_responses"
+        query_params = []
+        
+        # Add date range filter if dates are provided
+        if start_date and end_date and start_date != 'null' and end_date != 'null':
+            # Convert date strings to Unix timestamps
+            start_timestamp = int(datetime.strptime(start_date, '%Y-%m-%d').timestamp())
+            end_timestamp = int(datetime.strptime(end_date, '%Y-%m-%d').timestamp()) + (24 * 60 * 60)  # Add 24 hours
+            
+            query += " WHERE CAST(message_timestamp AS SIGNED) BETWEEN %s AND %s"
+            query_params.extend([start_timestamp, end_timestamp])
+        
+        # Add button_name filter if provided
+        if button_name:
+            from urllib.parse import unquote
+            button_name = unquote(button_name)
+            
+            if 'WHERE' in query:
+                query += " AND LOWER(message_body) LIKE LOWER(%s)"
+            else:
+                query += " WHERE LOWER(message_body) LIKE LOWER(%s)"
+            query_params.append(f"%{button_name}%")
+            
+        
+        # Execute query and log the results
+        cursor.execute(query, tuple(query_params))
+        rows = cursor.fetchall()
+        
+        # Define headers
+        headers = ['Date', 'display_phone_number', 'phone_number_id', 'waba_id',
+                   'contact_wa_id', 'status', 'message_timestamp', 'error_code',
+                   'error_message', 'contact_name', 'message_from', 'message_type',
+                   'message_body']
+        
+        df = pd.DataFrame(rows, columns=headers)
+        
+        # Create the HttpResponse object with CSV header
+        response = HttpResponse(content_type='text/csv')
+        
+        # Generate filename with date range if provided
+        if button_name:
+            if start_date != 'null' and end_date != 'null':
+                filename = f"report_{button_name}_{start_date}_to_{end_date}.csv"
+            else:
+                filename = f"report_{button_name}_complete.csv"
+                
+            response['Content-Disposition'] = f'attachment; filename="{filename}"'
+            
+            # Create CSV writer and write data
+            writer = csv.writer(response)
+            writer.writerow(headers)  # Write headers
+            writer.writerows(rows)    # Write all rows
+            return response
+        else:
+            return df
+            
+    except Exception as e:
+        logger.error(f"Error in download_linked_report: {str(e)}")
+        logger.error(f"Full error details: {str(e)}")
+        messages.error(request, "An error occurred while generating the report.")
+        return redirect('reports')
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            
+@login_required
+def download_campaign_report(request, report_id=None, insight=False, contact_list=None):
+    try:
+        # Fetch the specific report based on the report_id
+        if report_id:
+            report = get_object_or_404(ReportInfo, id=report_id)
+            Phone_ID = display_phonenumber_id(request)  # Ensure phone_number_id is defined
+            contacts = report.contact_list.split('\r\n')
+            contact_all = [phone.strip() for contact in contacts for phone in contact.split(',')]
+        else:
+            contact_all = contact_list
+
+        # Connect to the database
+        connection = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="fedqrbtb_wtsdealnow",
+            password="Solution@97",
+            database="fedqrbtb_report",
+            auth_plugin='mysql_native_password'
+        )
+        cursor = connection.cursor()
+        query = "SELECT * FROM webhook_responses"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        # Create a dictionary for quick lookup
+        rows_dict = {(row[2], row[4]): row for row in rows}
+        
+        matched_rows = []
+        non_reply_rows = []
+        
+        excluded_error_codes = {131048, 131000, 131042, 131031, 131053}
+    
+        if len(contact_all) > 100:
+            non_reply_rows = [row for row in rows if row[5] != "reply" and row[2] == Phone_ID and row[7] not in excluded_error_codes]
+        
+        for phone in contact_all:
+            matched = False
+            row = rows_dict.get((Phone_ID, phone), None)
+            if row:
+                matched_rows.append(row)
+                matched = True
+
+            if not matched and non_reply_rows:
+                new_row = copy.deepcopy(random.choice(non_reply_rows))
+                new_row_list = list(new_row)
+                new_row_list[4] = phone  # Update the phone number
+                new_row_tuple = tuple(new_row_list)
+                matched_rows.append(new_row_tuple)
+        
+        cursor.close()
+        connection.close()
+
+        # Define your header
+        header = [
+            "Date", "display_phone_number", "phone_number_id", "waba_id", "contact_wa_id",
+            "status", "message_timestamp", "error_code", "error_message", "contact_name",
+            "message_from", "message_type", "message_body"
+        ]
+
+        df = pd.DataFrame(matched_rows, columns=header)
+        status_counts_df = df['status'].value_counts().reset_index()
+        status_counts_df.columns = ['status', 'count']
+        total_unique_contacts = len(df['contact_wa_id'].unique())
+        total_row = pd.DataFrame([['Total Contacts', total_unique_contacts]], columns=['status', 'count'])
+        status_counts_df = pd.concat([status_counts_df, total_row], ignore_index=True)
+
+        # Generate CSV as HttpResponse (stream the file)
+        if insight:
+            return status_counts_df
+        elif contact_list:
+            return df
+        else:
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = f'attachment; filename="{report.campaign_title}.csv"'
+            
+            writer = csv.writer(response)
+            writer.writerow(header)  # Write header
+            writer.writerows(matched_rows)  # Write rows
+            
+            return response
+    
+    except mysql.connector.Error as err:
+        logger.error(f"Database error: {err}")
+        messages.error(request, "Database error occurred.")
+        return redirect('reports')
+
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {str(e)}")
+        messages.error(request, f"Error: {str(e)}")
+        return redirect('reports')
+    
+@login_required
+def get_report_insight(request, report_id):
+    try:
+        insight_data = download_campaign_report(request, report_id, insight=True)
+        return JsonResponse({
+            'status': 'success',
+            'data': insight_data.to_dict('records')
+        })
+    except Exception as e:
+        return JsonResponse({
+            'status': 'error',
+            'message': str(e)
+        }, status=500)
+        
+@login_required
+@require_http_methods(["DELETE"])
+def delete_report(request, report_id):
+    try:
+        report = ReportInfo.objects.get(id=report_id, email=request.user)
+        report.delete()
+        return JsonResponse({'message': 'Report deleted successfully!'}, status=200)
+    except ReportInfo.DoesNotExist:
+        return JsonResponse({'error': 'Report not found'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+    
+def get_latest_rows_by_contacts(contact_numbers):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="fedqrbtb_wtsdealnow",
+            password="Solution@97",
+            database="fedqrbtb_report",
+            auth_plugin='mysql_native_password'
+        )
+        
+        cursor = connection.cursor()
+        
+        query = """
+            SELECT r.Date, r.display_phone_number, r.phone_number_id, r.waba_id, r.contact_wa_id, 
+                   r.status, r.message_timestamp, r.error_code, r.error_message, r.contact_name, 
+                   r.message_from, r.message_type, r.message_body
+            FROM webhook_responses r
+            INNER JOIN (
+                SELECT contact_wa_id, MAX(message_timestamp) AS latest_message
+                FROM webhook_responses
+                WHERE contact_wa_id IN (%s)
+                GROUP BY contact_wa_id
+            ) latest 
+            ON r.contact_wa_id = latest.contact_wa_id AND r.message_timestamp = latest.latest_message
+            ORDER BY r.message_timestamp DESC
+        """ % ', '.join(['%s'] * len(contact_numbers))
+
+        cursor.execute(query, contact_numbers)
+        rows = cursor.fetchall()
+        columns = [
+            "Date", "display_phone_number", "phone_number_id", "waba_id", "contact_wa_id",
+            "status", "message_timestamp", "error_code", "error_message", "contact_name",
+            "message_from", "message_type", "message_body"
+        ]
+        
+        df = pd.DataFrame(rows, columns=columns)
+        cursor.close()
+        connection.close()
+        
+        return df
+    
+    except mysql.connector.Error as err:
+        print(f"Database error: {err}")
+        return None
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {str(e)}")
+        return None
+    
+def get_unique_phone_numbers():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="fedqrbtb_wtsdealnow",
+            password="Solution@97",
+            database="fedqrbtb_report",
+            auth_plugin='mysql_native_password'
+        )
+        cursor = connection.cursor()
+        query = """
+            SELECT DISTINCT contact_wa_id
+            FROM webhook_responses
+            WHERE status != 'failed';
+        """
+        cursor.execute(query)
+        unique_numbers = [row[0] for row in cursor.fetchall()]
+        cursor.close()
+        connection.close()
+
+        return unique_numbers
+
+    except mysql.connector.Error as err:
+        logger.error(f"Database error: {err}")
+        return []
+
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {str(e)}")
+        return []
