4189ee6be66cd2ec14c3eb109481ba374cc42eee
Prashanthg01 <prashanthprashanth8585@gmail.com>
2024-12-29 12:26:43 +0530

    updated UserAccess model

--- sms/smsapp/context_processors.py
+++ sms/smsapp/context_processors.py
@@ -7,14 +7,17 @@ def global_context(request):
         user_access = UserAccess.objects.get(user=request.user)
         api_doc_access = getattr(user_access, "can_access_API_doc", False)
         valid_num = getattr(user_access, "can_manage_number_validation", False)
+        twofauth_enable = getattr(user_access, "can_enable_2fauth", False)
         
         twofauth = Register_TwoAuth.objects.filter(user=request.user.username).exists()
     else:
         api_doc_access = None
         valid_num = None
         twofauth = False
+        twofauth_enable = False
     return {
         'api_doc_access': api_doc_access,
         'valid_num': valid_num,
         'twofauth': twofauth,
+        "twofauth_enable": twofauth_enable
     }
