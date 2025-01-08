cf97c4cf694d7f34c65e2e4dbcf4637c5f9a1cc0
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-01-08 13:12:15 +0530

    Reports Updated

--- sms/smsapp/views/reports.py
+++ sms/smsapp/views/reports.py
@@ -292,6 +292,9 @@ def download_campaign_report(request, report_id=None, insight=False, contact_lis
                     _ = send_validate_req(token, display_phonenumber_id(request), validate_req_num, "This is Just a testing message", report_id)
                 except Exception as e:
                     logger.error(f"Failed to call send_validate_req {str(e)}, {len(validate_req_num)} {report_id} {type(report_id)}")
+            else:
+                logger.info("No validate_req_num found")
+                update_start_id(report_id)
             try:
                 validation_data = get_latest_rows_by_contacts(no_match_num)
                 validation_data = validation_data[validation_data['error_code'] == 131026]
