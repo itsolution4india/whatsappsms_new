d71a22910b80915622b1237d313c1a8dc2af3b02
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-01-27 11:45:04 +0530

    updated session logout in authentication

--- sms/smsapp/middleware.py
+++ sms/smsapp/middleware.py
@@ -1,7 +1,6 @@
-from django.utils import timezone
-from django.contrib.auth import logout
+# middleware.py
+
 import logging
-from django.shortcuts import redirect
 
 logger = logging.getLogger('django.request')
 
@@ -16,20 +15,4 @@ class Log404DetailsMiddleware:
             # Log details of the 404 request
             logger.warning(f"404 Not Found: {request.path} - IP: {request.META.get('REMOTE_ADDR')} - User-Agent: {request.META.get('HTTP_USER_AGENT')}")
         
-        return response
-
-class AutoLogoutMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if request.user.is_authenticated:
-            last_activity = request.session.get('last_activity')
-            if last_activity:
-                inactive_duration = timezone.now() - last_activity
-                if inactive_duration.total_seconds() > 900:  # 15 minutes
-                    logout(request)
-                    return redirect('login')
-            request.session['last_activity'] = timezone.now()
-        response = self.get_response(request)
         return response
