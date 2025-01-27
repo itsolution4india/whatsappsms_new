6421ae0cbebc08f4239cf67074d4fc67c75394c3
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-01-27 12:16:58 +0530

    updated session logout in authentication

--- sms/smsapp/context_processors.py
+++ sms/smsapp/context_processors.py
@@ -1,23 +1,22 @@
-from .models import UserAccess
+from .models import UserAccess, Register_TwoAuth
 from .utils import logger
-from .models import Register_TwoAuth
 
 def global_context(request):
-    if request.user and str(request.user) != "AnonymousUser":
-        user_access = UserAccess.objects.get(user=request.user)
-        api_doc_access = getattr(user_access, "can_access_API_doc", False)
-        valid_num = getattr(user_access, "can_manage_number_validation", False)
-        twofauth_enable = getattr(user_access, "can_enable_2fauth", False)
-        
-        twofauth = Register_TwoAuth.objects.filter(user=request.user.username).exists()
-    else:
-        api_doc_access = None
-        valid_num = None
-        twofauth = False
-        twofauth_enable = False
-    return {
-        'api_doc_access': api_doc_access,
-        'valid_num': valid_num,
-        'twofauth': twofauth,
-        "twofauth_enable": twofauth_enable
+    context = {
+        'api_doc_access': None,
+        'valid_num': None,
+        'twofauth': False,
+        'twofauth_enable': False,
     }
+
+    if hasattr(request, 'user') and request.user.is_authenticated:
+        try:
+            user_access = UserAccess.objects.get(user=request.user)
+            context['api_doc_access'] = getattr(user_access, "can_access_API_doc", False)
+            context['valid_num'] = getattr(user_access, "can_manage_number_validation", False)
+            context['twofauth_enable'] = getattr(user_access, "can_enable_2fauth", False)
+            context['twofauth'] = Register_TwoAuth.objects.filter(user=request.user.username).exists()
+        except UserAccess.DoesNotExist:
+            logger.error(f"UserAccess not found for user: {request.user.username}")
+
+    return context
