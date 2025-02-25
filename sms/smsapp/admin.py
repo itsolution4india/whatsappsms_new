98feed65ddf3602a0fe84cd647e2cc6fbf15b085
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-02-25 21:27:08 +0530

    Testing dashboard

--- sms/smsapp/admin.py
+++ sms/smsapp/admin.py
@@ -74,8 +74,8 @@ class ReportInfoAdmin(admin.ModelAdmin):
         'message_delivery',
         "contact_list",
         "start_request_id",
-        "end_request_id"
-        
+        "end_request_id",
+        "waba_id_list"
         
 
     )
@@ -98,7 +98,8 @@ class ReportInfoAdmin(admin.ModelAdmin):
         'message_delivery',
         'template_name',
         "start_request_id",
-        "end_request_id"
+        "end_request_id",
+        "waba_id_list"
       
  
     )
