937b8ef9bc65d64ca2e672f337a9cbd78e4c6656
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-03-04 16:43:56 +0530

    updated backup data

--- sms/backup_webhook_responses.py
+++ sms/backup_webhook_responses.py
@@ -37,7 +37,7 @@ def execute_sql_file(filename):
 
 # Call the function to execute SQL file.
 if __name__ == "__main__":
-    execute_sql_file('webhook_responses.sql')
+    execute_sql_file('webhook_responses01.sql')
 
 # Close the database connection
 db_connection.close()
