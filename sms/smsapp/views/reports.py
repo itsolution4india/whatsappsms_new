1dd4b67e66945c13321812c335ab1d47341f9000
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-03-16 23:02:42 +0530

    updated reports

--- sms/smsapp/views/reports.py
+++ sms/smsapp/views/reports.py
@@ -627,7 +627,7 @@ def download_campaign_report2(request, report_id=None, insight=False, contact_li
                     new_row_tuple = tuple(new_row_list)
                     updated_matched_rows.append(new_row_tuple)
                 else:
-                    logger.info(f"No matched or non_reply_rows {non_reply_rows}")
+                    logger.info(f"No matched or non_reply_rows")
             else:
                 if not matched and non_reply_rows:
                     no_match_num.append(phone)
@@ -646,7 +646,7 @@ def download_campaign_report2(request, report_id=None, insight=False, contact_li
                     new_row_tuple = tuple(new_row_list)
                     updated_matched_rows.append(new_row_tuple)
                 else:
-                    logger.info(f"No matched or non_reply_rows {non_reply_rows}")
+                    logger.info(f"No matched or non_reply_rows")
                 
         
         response = HttpResponse(content_type='text/csv')
